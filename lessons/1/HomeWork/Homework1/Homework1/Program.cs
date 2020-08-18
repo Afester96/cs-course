b4f4e97d70438d0de67c3218018011f0b30828c0
Afester <Afester@mail.ru>
2020-08-18 14:51:44 +0300

    Created a file and completed the task

---
+++ lessons/1/HomeWork/Homework1/Homework1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Переменная для запоминания имени пользователя.
+            string name;
+
+            //Отправка сообщения пользователю.
+            Console.WriteLine("Please, enter your name");
+
+            //Ввод имени и его запоминание.
+            name = Console.ReadLine();
+
+            //Информирование пользователя о том, что программа не зависла.
+            Console.WriteLine("Thank you! Please, await 5 seconds.");
+
+            //Ожидание 5 секунд.
+            Thread.Sleep(5000);
+
+            //Приветствие пользателя. С использованием данных полученных при вводе.
+            Console.WriteLine("Welcome, " + name + "! Please, await 5 seconds.");
+
+            //Ожидание 5 секунд.
+            Thread.Sleep(5000);
+
+            //Прощание с пользователем. С использованием данных полученных при вводе.
+            Console.WriteLine("Goodbye, " + name + "! Come back again.");
+
+            //Ожидание нажатия кнопки пользователем.
+            Console.ReadLine();
+        }
+    }
+}
