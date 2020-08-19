46b72af4fb7ffbc762aad0f875520b7b5e623b5f
Afester <Afester@mail.ru>
2020-08-19 21:39:41 +0300

    End

--- lessons/2/ClassWork/ClassWork1/ClassWork1/Program.cs
+++ lessons/2/ClassWork/ClassWork1/ClassWork1/Program.cs
@@ -6,7 +6,18 @@ namespace ClassWork1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            name = "Afester";
+
+            string name1 = "Afesters";
+            string name2 = "Afesters";
+            char Variable = 'A';
+            
+            int x = 10_000_000;
+            Console.WriteLine(1.0f/3.0f);
+            Console.WriteLine(1.0d / 3.0d);
+            Console.WriteLine(1.0M/3.0M);
+            //Сниппет
         }
     }
 }
