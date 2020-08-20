1c4d93c228bfdf5257dc8e3e2b774ff078027441
Afester <Afester@mail.ru>
2020-08-20 12:04:26 +0300

    Provided more information to the user

--- lessons/2/HomeWork/HomeWork2Hard/HomeWork2Hard/Program.cs
+++ lessons/2/HomeWork/HomeWork2Hard/HomeWork2Hard/Program.cs
@@ -10,7 +10,9 @@ namespace HomeWork2Hard
             Console.WriteLine("Please, enter two numbers.");
 
             //Converting entered string numbers into double numbers
+            Console.WriteLine("First number");
             double numberFirst = double.Parse(Console.ReadLine());
+            Console.WriteLine("Second number");
             double numberSecond = double.Parse(Console.ReadLine());
 
             //Providing information to the user about mathematical operation
@@ -22,28 +24,28 @@ namespace HomeWork2Hard
             //Providing information about mathematical operations with if else construction
             if (mathematicalOpreation == "+") //addition operation
             {
-                Console.WriteLine("addition");
-                Console.WriteLine(numberFirst + numberSecond);
+                Console.WriteLine("Addition");
+                Console.WriteLine("Your result is " + (numberFirst + numberSecond));
             }
             else if (mathematicalOpreation == "-") //subtraction operation
             {
-                Console.WriteLine("subtraction");
-                Console.WriteLine(numberFirst - numberSecond);
+                Console.WriteLine("Subtraction");
+                Console.WriteLine("Your result is " + (numberFirst - numberSecond));
             }
             else if (mathematicalOpreation == "*") //multiplication operation
             {
-                Console.WriteLine("multiplication");
-                Console.WriteLine(numberFirst * numberSecond);
+                Console.WriteLine("Multiplication");
+                Console.WriteLine("Your result is " + (numberFirst * numberSecond));
             }
             else if (mathematicalOpreation == "/") //division operation
             {
-                Console.WriteLine("division");
-                Console.WriteLine(numberFirst / numberSecond);
+                Console.WriteLine("Division");
+                Console.WriteLine("Your result is " + (numberFirst / numberSecond));
             }
             else //operation if user write wrong mathematical operation
             {
                 Console.WriteLine("Plese enter correct mathematical operation (+, -, *, /)");
-            }
+            }           
 
             //Providing information about the end of program and opportunity to exit
             Console.WriteLine("Thank you for using our program, press any key for exit!");
