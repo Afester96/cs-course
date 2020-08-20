37c242b3781d77fde09637cde8ae7aee16aa44eb
Afester <Afester@mail.ru>
2020-08-20 11:36:27 +0300

    Finished homework

---
+++ lessons/2/HomeWork/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Providing information to the user
+            Console.WriteLine("Please, enter two numbers.");
+
+            //Converting entered string into double
+            double numberFirst = double.Parse(Console.ReadLine());
+            double numberSecond = double.Parse(Console.ReadLine());
+
+            //Providing information about mathematical operations
+            Console.WriteLine("addition");
+            Console.WriteLine(numberFirst + numberSecond);
+            Console.WriteLine("subtraction");
+            Console.WriteLine(numberFirst - numberSecond);
+            Console.WriteLine("multiplication");
+            Console.WriteLine(numberFirst * numberSecond);
+            Console.WriteLine("division");
+            Console.WriteLine(numberFirst / numberSecond);
+        }
+    }
+}
