1d2e195dfa2f7a95ee009ed2436f4e7b1746768b
Afester <Afester@mail.ru>
2020-08-24 21:33:52 +0300

    end

---
+++ lessons/3/ClassWork/ClassWork3/ClassWork3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace ClassWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+           
+            for (int i = 0; i < 5; i++)
+            {
+               Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
