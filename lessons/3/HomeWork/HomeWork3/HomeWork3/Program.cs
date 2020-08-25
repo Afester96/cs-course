962c89fd7d6472556011f665940553a8dffda9b1
Afester <Afester@mail.ru>
2020-08-25 21:51:06 +0300

    Homework3

---
+++ lessons/3/HomeWork/HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User greeting
+            var computerName = Environment.MachineName.ToString();
+            Console.WriteLine("Hello " + computerName + "!");
+
+            //Create string arrays for further use in for loops.
+            //namePresent is responsible for the name entered into the console.
+            //agePresent is responsible for the age entered in the console.
+            string[] namePresent = new string[3];
+            int[] agePresent = new int[namePresent.Length];
+            int[] ageFuture = new int[agePresent.Length];
+            
+
+            //Creating a for loop to add data to array
+            for (int i = 0; i < namePresent.Length; i++)
+            {
+                Console.WriteLine("Enter name № " + (i+1) + "."); 
+                namePresent[i] = Console.ReadLine();
+                Console.WriteLine("Enter age " + namePresent[i] + ".");
+                agePresent[i] = int.Parse(Console.ReadLine());
+            }
+            
+            //Convert string to int and then sum with sumAge
+            for (int i = 0; i < namePresent.Length; i++)
+            {
+                Console.WriteLine("How much do you want to increase " + namePresent[i] + " age?");
+                int ageNumber = int.Parse(Console.ReadLine());
+                ageFuture[i] = ageNumber + agePresent[i]; 
+                Console.WriteLine("After " + ageNumber + " years " + namePresent[i] + " will be " + ageFuture[i] + " years old.");                
+            }
+
+            //End of the program
+            Console.WriteLine("Press anything to end the program.");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ lessons/3/HomeWork/HomeWork3TEST/HomeWork3TEST/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWork3TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age1 = 5;
+            int age2 = 6;
+            int ageSum = age1 + age2;
+            int ageSum2 = age1 + age2;
+            int ageSum3 = age1 + age2;
+            Console.WriteLine(ageSum + ageSum2 + ageSum3);
+        }
+    }
+}
