9737dc1fa6cbbd87e9ff24eeb648b65c7adfb232
Afester <Afester@mail.ru>
2020-08-28 20:35:58 +0300

    Added rest colors

--- lessons/4/ClassWork/ClassWork4/ClassWork4/Program.cs
+++ lessons/4/ClassWork/ClassWork4/ClassWork4/Program.cs
@@ -2,35 +2,48 @@
 
 namespace ClassWork4
 {
-    enum colours
+    [Flags]
+    enum colors
     {
-        black = 0b1,
-        blue = 0b1 << 1,
-        cyan = 0b1 << 2,
-        grey = 0b1 << 3,
-        green = 0b1 << 4,
-        magenta = 0b1 << 5,
-        red = 0b1 << 6,
-        white = 0b1 << 7,
-        yellow = 0b1 << 8
+        zero = 0,
+        black = 0x1,
+        blue = 0x2,
+        cyan = 0x4,
+        grey = 0x8,
+        green = 0x10,
+        magenta = 0x20,
+        red = 0x40,
+        white = 0x80,
+        yellow = 0x100
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Вот все значения. выбери какой нравится");
-            var coloursArray = (colours[]) Enum.GetValues(typeof(colours));
-            for (int i = 0; i < coloursArray.Length; i++)
+
+            colors allColors = colors.zero;
+            var colorsGetNames = Enum.GetNames(typeof(colors));
+                        
+            for (var i = 0; i < colorsGetNames.Length; i++)
             {
-                Console.WriteLine(coloursArray[i]);
+                allColors |= (colors)(1 << i);                
             }
+
+            Console.WriteLine("Here are all colors. Please, choose 4 colors to add they to favorites.");
+            for (int i = 1; i < colorsGetNames.Length; i++)
+            {
+                Console.WriteLine(colorsGetNames[i]);
+            }
+            colors favoriteColors = colors.zero;
             for (int i = 0; i < 4; i++)
             {
-                colours colour = (colours) Enum.Parse (typeof(colours), Console.ReadLine());
-                colours  = colour | coloursArray[i];
-                Console.WriteLine();
+                Console.Write("Enter the color: ");
+                var color = (colors)Enum.Parse(typeof(colors), Console.ReadLine());
+                favoriteColors |= color;
             }
-            
+                                    
+            Console.WriteLine("Your favorite colors are: " + favoriteColors);
+            Console.WriteLine("Rest colors are: " + (favoriteColors ^ allColors) );
             
         }
     }
