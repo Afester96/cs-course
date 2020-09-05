5ac8718ccba7d32829a3f57c8fab60cced8ec334
Afester <Afester@mail.ru>
2020-09-05 13:52:57 +0300

    First done

---
+++ lessons/6/HomeWork/HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int enteredNumber = ReadNumber("Введите положительное натуральное число не более 2 миллиардов");
+            var sEnteredNumber = enteredNumber.ToString();
+            int evenCounter = 0;
+            for (int i = 0; i < sEnteredNumber.Length; i++)
+            {
+                if ((int.Parse(sEnteredNumber[i].ToString()) % 2) == 0)
+                    evenCounter++;
+            }
+            Console.WriteLine("В числе " + enteredNumber + " содержится " + evenCounter + " чётных чисел.");
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+        static int ReadNumber(string text)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(text);
+                    int readNumber = int.Parse(Console.ReadLine());
+                    if (readNumber >= 0)
+                    {
+                        return readNumber;
+                    }
+                    else if (readNumber < 0)
+                    {
+                        Console.WriteLine("Ошибка! Введено неверное значение. Попробуйте ещё раз: ");
+                        continue;
+                    }
+                    else if (readNumber < 2000000000)
+                    {
+                        Console.WriteLine("Ошибка! Введено неверное значение. Попробуйте ещё раз: ");
+                        continue;
+                    }
+                }
+                catch (FormatException exception)
+                {
+                    Console.WriteLine("Ошибка " + exception.Message + "! Попробуйте ещё раз: ");
+                }
+                catch (OverflowException exception)
+                {
+                    Console.WriteLine("Ошибка " + exception.Message + "! Попробуйте ещё раз: ");
+                }
+            }
+        }
+    }
+}
