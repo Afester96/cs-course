7cbe49e2f7ef6f8832e2df8105de81aa0474590d
Afester <Afester@mail.ru>
2020-10-29 12:21:26 +0300

    feat: changed File.WriteAllText to File.OpenWrite

--- lessons/17/HomeWork/HomeWork17/HomeWork17/FileWriterWithProgress.cs
+++ lessons/17/HomeWork/HomeWork17/HomeWork17/FileWriterWithProgress.cs
@@ -10,27 +10,22 @@ namespace HomeWork17
 
         public void WriteBytes(string fileName, byte[] data, float percentageToFireEvent)
         {
-            var dataFirstValue = 0;
-            var dataArrayCount = 0;
-            byte[] dataArray = new byte[(int)(data.Length * percentageToFireEvent / 1.0)];
             for (int i = 0; i < data.Length; i++)
             {
                 if (i != 0 && i % ((int)(data.Length * percentageToFireEvent / 1.0)) == 0)
                 {
-                    var dataSecondValue = i;
-                    dataArrayCount = 0;
-                    for (int j = dataFirstValue; j < dataSecondValue; j++)
+                    using (FileStream fs = File.OpenWrite(fileName))
                     {
-                        dataArray[dataArrayCount] = data[j];
-                        dataArrayCount++;
+                        fs.Write(data, 0, i);
                     }
-                    File.WriteAllText($"{i / 100 + "% " + fileName}", String.Join(" ", dataArray));
-                    dataFirstValue = dataSecondValue;
                     WritingPerformed?.Invoke(this, new WritingPerformedEventArgs($"{fileName}", data, i / 100));
                 }
             }
             WritingCompleted?.Invoke(this, new WritingCompletedEventArgs("Writing Complited!"));
-            File.WriteAllText($"{100 + "% " + fileName}", String.Join(" ", data));
+            using (FileStream fs = File.OpenWrite(fileName))
+            {
+                fs.Write(data, 0, data.Length);
+            }
         }
     }
 }
